5d82570a360c204713c8710c9af0bfd6dab5c698
Hoang Ho <taxvui@gmail.com>
2026-05-20 20:49:22 +0700

    maxui.net

--- lib/app/core/static/app_config/_app_config.dart
+++ lib/app/core/static/app_config/_app_config.dart
@@ -1,11 +1,11 @@
 abstract class AppConfig {
   static const String appName = 'Restaurant App';
   static const String appVersion = 'V-2.0.0';
-  static const String orgDomain = 'acnoo.com';
+  static const String orgDomain = 'maxui.net';
 
   ///[baseUrl] must be the backend's base url `WITHOUT ANY TRAILING SLASH(/)`
   static const String baseUrl = 'https://pos.maxui.net';
 
   // Purchase Code
-  static const String purchaseCode = 'YOUR_PURCHASE_CODE';
+  static const String purchaseCode = '12345';
 }
